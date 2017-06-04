489cf2dc1a9bbf677bab5965145a1f16b5da68a1
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-06-05 01:40:37 +0900

    feat: agreed-server & agreed-client are updated to v2.0

--- test/bin/not_passed.js
+++ test/bin/not_passed.js
@@ -5,9 +5,9 @@ const cp = require('child_process');
 const pass = './test/agreed.json5';
 const not_pass = './test/not_passed.json5';
 plzPort().then((port) => {
-  const proc = cp.exec(`node ${process.cwd()}/bin/agreed-server.js --port ${port} --path ${not_pass}`);
+  const proc = cp.exec(`${process.cwd()}/node_modules/.bin/agreed-server --port ${port} --path ${not_pass}`);
   setTimeout(() => {
-    const result = cp.execSync(`node ${process.cwd()}/bin/agreed-client.js --port ${port} --path ${pass}`).toString();
+    const result = cp.execSync(`${process.cwd()}/node_modules/.bin/agreed-client --port ${port} --path ${pass}`).toString();
 
     console.log(result);
     assert(result.indexOf('✗ fail') >= 0);
