76bc04737130b62f3b18d3c795feb47f15a92f54
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-06-13 19:33:59 +0900

    feat: agreed randomInt for response randomize text

---
+++ test/lib/server.randomInt.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const agreedServer = require('../helper/server.js');
+const test = require('eater/runner').test;
+const http = require('http');
+const AssertStream = require('assert-stream');
+const assert = require('power-assert');
+const mustCall = require('must-call');
+
+test('server: check randomInt', () => {
+  const server = agreedServer({
+    path: 'test/agrees/agrees.json5',
+    port: 0,
+  });
+
+  server.on('listening', () => {
+    const options = {
+      host: 'localhost',
+      method: 'GET',
+      path: '/test/randomint/agreed/values',
+      port: server.address().port,
+    };
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (d) => data += d);
+      res.on('end', mustCall(() => {
+        const result = JSON.parse(data);
+        assert(result.random >= 0);
+        assert(result.random < 1000);
+      }));
+      server.close();
+    }).on('error', console.error);
+
+    req.end();
+  });
+});
+
