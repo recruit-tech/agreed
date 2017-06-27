76657a1941f926bf8c43cd5ac7ebb6a944b74bb0
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-06-27 16:58:45 +0900

    fix(template): fix nested object bind like {:value.foo.bar}

--- test/lib/template/bind.js
+++ test/lib/template/bind.js
@@ -18,3 +18,14 @@ test('bind: { key: { foo: "{:value}" }, { key: { foo: null } } => { value: null 
   const result = bind({ key: { foo: "{:value}" }}, { key: { foo: null }});
   assert.deepEqual(result, {value: null });
 });
+
+test('bind: { key: { foo: "{:value.foo}" }, { key: { value: { foo: 12345 } } } => { value: { foo: 12345 } }', () => {
+  const result = bind({ key: { foo: "{:value.foo}" }}, { key: { value: { foo: 12345 } } });
+  assert.deepEqual(result, {value: { foo: 12345 } });
+});
+
+test('bind: { key: { foo: "{:value.foo.bar.0.baz}" }, { key: { value: { foo: { bar: [ { baz : 12345 } ] } } } } => { value: { foo: { bar: [ { baz: 12345 } ] } } }', () => {
+  const result = bind({ key: { foo: "{:value.foo.bar.0.baz}" } }, { key: { value: { foo: { bar: [ { baz : 12345 } ] } } } });
+  const expect = { value: { foo: { bar: [ { baz: 12345 } ] } } };
+  assert.deepStrictEqual(result, expect);
+});
