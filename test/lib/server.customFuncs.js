465e4a1264750012c2b15ce67a6f627ca0f6992a
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-08-24 12:47:29 +0900

    feat(operation): add custom operation functions

---
+++ test/lib/server.customFuncs.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const agreedServer = require('../helper/server.js');
+const test = require('eater/runner').test;
+const http = require('http');
+const AssertStream = require('assert-stream');
+const assert = require('power-assert');
+const mustCall = require('must-call');
+
+test('server: check custom function', () => {
+  const server = agreedServer({
+    agrees: [{
+      request: {
+        path: '/test/custom/agreed/values',
+        query: {
+          a: '{:a}',
+          b: '{:b}',
+          c: '{:c}',
+        },
+        values: {
+          a: 1,
+          b: 2,
+          c: 3,
+        },
+      },
+      response: {
+        body: {
+          sum: '{sum:a,b,c}',
+        },
+        funcs: {
+          sum: (a, b, c) => parseInt(a) + parseInt(b) + parseInt(c)
+        }
+      },
+    }],
+    port: 0,
+  });
+
+  server.on('listening', () => {
+    const options = {
+      host: 'localhost',
+      method: 'GET',
+      path: '/test/custom/agreed/values?a=1&b=2&c=4',
+      port: server.address().port,
+    };
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (d) => data += d);
+      res.on('end', mustCall(() => {
+        const result = JSON.parse(data);
+        assert.strictEqual(result.sum, 7);
+      }));
+      server.close();
+    }).on('error', console.error);
+
+    req.end();
+  });
+});
+
