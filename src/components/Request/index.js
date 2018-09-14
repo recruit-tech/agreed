d5c01c58543a123a4333350aea982030360dd346
新井　智士 <01017830@CA3249.local>
2018-09-14 18:52:35 +0900

    fix: queries not displayed

--- src/components/Request/index.js
+++ src/components/Request/index.js
@@ -4,12 +4,22 @@ import Definitions from '../Definitions'
 import Body from '../Body'
 
 const Request = ({ data }) => {
-  const { method, path, headers_formatted: headers, formatted, schema } = data
+  const { method, path, headers_formatted: headers, query, formatted, schema } = data
   return (
     <div>
       <Definitions title="method" description={method.toUpperCase()} />
       <Definitions title="path" description={path} />
-      {!!Object.keys(headers).length && (
+      {query && !!Object.keys(query).length && (
+        <Definitions title="query">
+          {Object.entries(query).map(([k, v]) => (
+            <dl key={k}>
+              <dt>{k}</dt>
+              <dd>{JSON.stringify(v)}</dd>
+            </dl>
+          ))}
+        </Definitions>
+      )}
+      {headers && !!Object.keys(headers).length && (
         <Definitions title="headers">
           {Object.entries(headers).map(([k, v]) => (
             <dl key={k}>
