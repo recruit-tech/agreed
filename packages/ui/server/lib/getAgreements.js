897618f4e5c594ce6cd42cc53669e0cb05cd6637
yosuke-furukawa <yosuke.furukawa@gmail.com>
2024-03-14 19:09:14 +0900

    update agreed/ui to pass lint

--- packages/ui/server/lib/getAgreements.js
+++ packages/ui/server/lib/getAgreements.js
@@ -7,29 +7,25 @@ const requireUncached = require('@agreed/core/lib/require_hook/requireUncached')
 const format = require('@agreed/core/lib/template/format')
 const { parseSchema } = require('json-schema-to-flow-type')
 
-module.exports = function(options) {
-  const agreesPath = path.resolve(options.path)
+module.exports = function (options) {
+  const agreesPath = path.resolve(options.path ?? '')
   const base = path.dirname(agreesPath)
 
   register()
-
   const agrees = [].concat(requireUncached(agreesPath))
-  return agrees.map((agree) => completion(agree, base)).map((agree) => {
-    agree.request.headers_formatted = format(
-      agree.request.headers,
-      agree.request.values
-    )
-    agree.request.formatted = format(agree.request.body, agree.request.values)
 
-    agree.response.formatted = format(
-      agree.response.body,
-      agree.response.values
-    )
+  return agrees
+    .map((agree) => completion(agree, base))
+    .map((agree) => {
+      agree.request.headers_formatted = format(agree.request.headers, agree.request.values)
+      agree.request.formatted = format(agree.request.body, agree.request.values)
 
-    if (agree.response.schema) {
-      agree.response.flowtype = parseSchema(agree.response.schema)
-    }
+      agree.response.formatted = format(agree.response.body, agree.response.values)
 
-    return agree
-  })
+      if (agree.response.schema) {
+        agree.response.flowtype = parseSchema(agree.response.schema)
+      }
+
+      return agree
+    })
 }
--- packages/ui/src/components/Body/index.js
+++ packages/ui/src/components/Body/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import JSONTree from 'react-json-tree'
+import { JSONTree } from 'react-json-tree'
 import classNames from 'classnames'
 import './styles.css'
 
